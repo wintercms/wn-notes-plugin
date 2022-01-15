03db1b30c08f692b91a99e555c3e9fb663ed5884
WebVPF <61043464+WebVPF@users.noreply.github.com>
2022-01-15 20:28:40 +0200

    Added Russian translation (#1)

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -7,11 +7,16 @@
         'note' => [
             'label' => 'Note',
             'label_plural' => 'Notes',
-            'name'  => 'Title',
+            'name' => 'Title',
+            'new_note' => 'New Note',
+            'add_new_note' => 'Add new note',
+            'delete_note' => 'Delete note',
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
             'deleted_at' => 'Deleted At',
             'delete_confirm' => 'Are you sure you want to delete the selected note?',
+            'saving' => 'Saving',
+            'no_notes_to_display' => 'No notes to display',
         ],
     ],
 ];
