da4e5cee88773e83ea1f0103bf4c61af66675585
Nelson Otazo <notazo@applaudostudios.com>
2022-01-17 16:56:44 -0300

    Added Spanish translation (#5)
    Credit to @norotaro

---
+++ lang/es/lang.php
@@ -0,0 +1,22 @@
+<?php return [
+    'plugin' => [
+        'name'        => 'Notas',
+        'description' => 'Agregue notas fácilmente a cualquier registro en WinterCMS',
+    ],
+    'models' => [
+        'note' => [
+            'label' => 'Nota',
+            'label_plural' => 'Notas',
+            'name' => 'Título',
+            'new_note' => 'Nueva Nota',
+            'add_new_note' => 'Agregar nueva nota',
+            'delete_note' => 'Eliminar nota',
+            'created_at' => 'Creado El',
+            'updated_at' => 'Actualizado El',
+            'deleted_at' => 'Eliminado El',
+            'delete_confirm' => '¿Está seguro de que desea eliminar la nota seleccionada?',
+            'saving' => 'Guardando',
+            'no_notes_to_display' => 'No hay notas para mostrar',
+        ],
+    ],
+];
