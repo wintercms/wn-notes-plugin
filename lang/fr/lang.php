426b1bf4297b46f7fad35a40e56f4f8bfbebf74e
Damien MATHIEU <contact@hounddd.fr>
2022-01-17 17:00:32 +0100

    Added French Translation (#4)
    Credit to @damsfx

---
+++ lang/fr/lang.php
@@ -0,0 +1,22 @@
+<?php return [
+    'plugin' => [
+        'name'        => 'Notes',
+        'description' => "Ajouter facilement des notes à n'importe quel enregistrement dans WinterCMS",
+    ],
+    'models' => [
+        'note' => [
+            'label' => 'Note',
+            'label_plural' => 'Notes',
+            'name' => 'Titre',
+            'new_note' => 'Nouvelle Note',
+            'add_new_note' => 'Ajouter une note',
+            'delete_note' => 'Supprimer une note',
+            'created_at' => 'Créé le',
+            'updated_at' => 'Mis à jour le',
+            'deleted_at' => 'Supprimé le',
+            'delete_confirm' => 'Êtes-vous sûr de vouloir supprimer la note sélectionnée ?',
+            'saving' => 'Enregistrement',
+            'no_notes_to_display' => 'Pas de notes à afficher',
+        ],
+    ],
+];
