39b999c3491ff775fc04e24d59d0469e308c0346
Web-VPF <pavrub@yandex.ru>
2022-02-06 13:17:35 +0200

    The interface is translated

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -17,6 +17,10 @@
             'delete_confirm' => 'Are you sure you want to delete the selected note?',
             'saving' => 'Saving',
             'no_notes_to_display' => 'No notes to display',
+            'has_been_saved' => 'Note has been saved',
+            'has_been_deleted' => 'Note has been deleted',
+            'finish_saving_before_add_new' => 'Finish saving this note before you can add a new one',
+            'must_exist' => 'Note must exist before it can be deleted',
         ],
     ],
 ];
